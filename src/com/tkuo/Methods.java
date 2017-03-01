c3a2849861ca7b127455267062a752483cc42724
jiffkuo <jiffkuo@gmail.com>
2017-02-28 22:08:23 -0800

    complete pearson correlation

--- src/com/tkuo/Methods.java
+++ src/com/tkuo/Methods.java
@@ -11,19 +11,21 @@ import java.util.List;
  *   - Cosine Vector Similarity
  *   - Pearson Correlation
  */
+
 public class Methods {
 
     // private data variable
-    // store weight for each user (userID: 1 - 200)
+    // store weight and average rating for each user (userID: 1 - 200)
     private List<Double> weightList;
-    private double       activeUserAvgRating;
     private List<Double> similarUsersAvgRating;
+    private double       activeUserAvgRating;
 
     // Support two methods
     public enum MethodType {
         CosVecSim,
         PearsonCorr
     }
+
     // constructor
     public Methods() {
         weightList = new ArrayList<>();
@@ -78,6 +80,7 @@ public class Methods {
             System.err.println("[Error] Cannot calculate Pearson Correction weight");
             return 0.0;
         }
+
         // calculate rating average
         for (int i = 0; i < lenVec; i++) {
             avgVec1 += vec1.get(i);
@@ -100,8 +103,14 @@ public class Methods {
         if (dotProduct == 0) {
             return 0;
         }
-        // return weight
-        return dotProduct / (Math.sqrt(norm1) * Math.sqrt(norm2));
+        // return weight [-1, 1]
+        double result = dotProduct / (Math.sqrt(norm1) * Math.sqrt(norm2));
+        if (result < -1) {
+            return -1;
+        } else if (result > 1) {
+            return 1;
+        }
+        return result;
     }
 
     // Use specific method to calculate weight for each user
@@ -110,7 +119,7 @@ public class Methods {
         // vec1: active user, vec2: similar user
         ArrayList<Integer> vec1;
         ArrayList<Integer> vec2;
-        // clear average rating record
+        // initial average rating record
         activeUserAvgRating = 3;
         similarUsersAvgRating.clear();
 
@@ -144,7 +153,7 @@ public class Methods {
         // System.out.println("[Info] Total " + weightList.size() + " users are weighted");
     }
 
-    // Based on weight information to do rating prediction
+    // Based on Cosine Similarity weight information to do rating prediction
     public double PredictByCosVecSim(List<HashMap<Integer, Integer>> trainDataSet, int movieID) {
         // default prediction rating = 3 if no relevant info
         double predictRating = 3;
@@ -181,7 +190,7 @@ public class Methods {
         return predictRating;
     }
 
-    // Based on weight information to do rating prediction
+    // Based on Pearon Correlation weight information to do rating prediction
     public double PredictByPearsonCorr(List<HashMap<Integer, Integer>> trainDataSet, int movieID) {
         // set active user average rating if no relevant info
         double predictRating = activeUserAvgRating;
@@ -198,7 +207,7 @@ public class Methods {
             System.err.print(" Size of AvgRating = " + similarUsersAvgRating.size());
             return 0;
         }
-        // Rating prediction (Cosine Similarity)
+        // Rating prediction (Predict Correlation)
         // Assume all users are the most similar users
         for (int user = 0; user < trainDataSet.size(); user++) {
             HashMap<Integer, Integer> traindata = trainDataSet.get(user);
@@ -207,10 +216,6 @@ public class Methods {
             } else {
                 System.err.println("[Error] No record (userID: " + (user+1) + "movieID: " + movieID + ")");
                 rating = 0; // no rating record
-            }
-            // TODO: need to exam it
-            if (rating == 0) {
-                continue;
             }
 
             totalWeight += Math.abs(weightList.get(user));
